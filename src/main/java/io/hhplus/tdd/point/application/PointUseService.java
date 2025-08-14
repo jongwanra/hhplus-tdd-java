16f5580abf77d182ab31b23754b7fa1d20449155
jongwanra <najongsi@gmail.com>
2025-08-14 18:27:51 +0900

    feat: 포인트 사용 기능 동시성 테스트

--- src/main/java/io/hhplus/tdd/point/application/PointUseService.java
+++ src/main/java/io/hhplus/tdd/point/application/PointUseService.java
@@ -1,5 +1,11 @@
 package io.hhplus.tdd.point.application;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import org.springframework.stereotype.Service;
 
 import io.hhplus.tdd.database.PointHistoryTable;
@@ -13,18 +19,40 @@ import lombok.RequiredArgsConstructor;
 public class PointUseService {
 	private final UserPointTable userPointTable;
 	private final PointHistoryTable pointHistoryTable;
+	private final Map<Long, Lock> userIdToLockMap = new ConcurrentHashMap<>();
 
 	public UserPoint execute(Command command) {
-		UserPoint userPoint = userPointTable.selectById(command.userId)
-			.use(command.amount, command.currentTimeMillis);
+		Lock lock = userIdToLockMap.computeIfAbsent(command.userId, (userId) -> new ReentrantLock(true));
+		tryLock(lock);
+
+		UserPoint userPoint;
+		try {
+			UserPoint usedUserPoint = userPointTable.selectById(command.userId)
+				.use(command.amount, command.currentTimeMillis);
+			userPoint = userPointTable.insertOrUpdate(usedUserPoint.id(), usedUserPoint.point());
+		} finally {
+			lock.unlock();
+		}
 
 		pointHistoryTable.insert(command.userId, command.amount, TransactionType.USE, command.currentTimeMillis);
-		return userPointTable.insertOrUpdate(userPoint.id(), userPoint.point());
+		
+		return userPoint;
+	}
+
+	private void tryLock(Lock lock) {
+		try {
+			if (!lock.tryLock(10, TimeUnit.SECONDS)) {
+				throw new RuntimeException("잠시 후에 다시 시도해 주시기 바랍니다.");
+			}
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public record Command(
 		long userId,
 		long amount,
 		long currentTimeMillis
-	) {}
+	) {
+	}
 }
--- src/test/java/io/hhplus/tdd/point/application/PointUseServiceIntegrationTest.java
+++ src/test/java/io/hhplus/tdd/point/application/PointUseServiceIntegrationTest.java
@@ -16,66 +16,70 @@ import io.hhplus.tdd.database.UserPointTable;
 import io.hhplus.tdd.point.domain.PointHistory;
 import io.hhplus.tdd.point.domain.UserPoint;
 
-public class PointChargeServiceIntegrationTest {
-	private PointChargeService pointChargeService;
+class PointUseServiceIntegrationTest {
+	private PointUseService pointUseService;
 	private UserPointTable userPointTable;
 	private PointHistoryTable pointHistoryTable;
 
 	@BeforeEach
 	void setUp() {
-		userPointTable = new UserPointTable();
 		pointHistoryTable = new PointHistoryTable();
-		// pointChargeService = new NonThreadSafePointChargeService(pointHistoryTable, userPointTable);
-		// pointChargeService = new SynchronizedPointChargeService(pointHistoryTable, userPointTable);
-		// pointChargeService = new SynchronizedKeywordPointChargeService(pointHistoryTable, userPointTable);
-		pointChargeService = new ReentrantLockedPointChargeService(pointHistoryTable, userPointTable);
+		userPointTable = new UserPointTable();
+		pointUseService = new PointUseService(userPointTable, pointHistoryTable);
 	}
 
 	/**
 	 * [작성 이유]
-	 * 동일한 회원이 동시에 10번 포인트를 충전하려고 했을 때, 동시성 이슈 발생 여부를 확인하고자 작성했습니다.
+	 * 동일한 회원이 동시에 10번 포인트를 사용하려고 했을 때, 동시성 이슈 발생 여부를 확인하고자 작성했습니다.
 	 */
 
 	@Test
-	void 동일한_회원이_동시에_10번_1000_포인트_충전을_요청할_경우_10_000원을_반환한다() throws InterruptedException {
+	void 회원이_가지고_있는_10000_포인트를_1000_포인트씩_동시에_10번_사용할_경우_최종적으로_포인트는_0원이_된다() throws InterruptedException {
 		// given
 		final int threadCount = 10;
 		final long userId = 123L;
 		final long amount = 1000L;
-
 		CountDownLatch countDownLatch = new CountDownLatch(threadCount);
 		ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+
+		userPointTable.insertOrUpdate(userId, threadCount * 1_000L);
 
 		// when
 		IntStream.range(0, threadCount)
 			.forEach((index) -> executorService.execute(() -> {
-					pointChargeService.execute(
-						new PointChargeService.Command(userId, amount, System.currentTimeMillis()));
-					countDownLatch.countDown();
-				}
+				pointUseService.execute(new PointUseService.Command(
+					userId, amount, System.currentTimeMillis()
+				));
+				countDownLatch.countDown();
+			}));
 
-			));
 		countDownLatch.await();
 
 		// then
 		UserPoint userPoint = userPointTable.selectById(userId);
-		assertThat(userPoint.point()).isEqualTo(1_000L * threadCount);
+		assertThat(userPoint.point()).isZero();
 
 		List<PointHistory> pointHistories = pointHistoryTable.selectAllByUserId(userId);
 		assertThat(pointHistories.size()).isEqualTo(threadCount);
-
 	}
 
 	/**
 	 * [작성 이유]
-	 * 서로 다른 회원 10명이 동시에 포인트를 충전하려고 접근했을 떄 동시성 이슈가 발생하는지 여부를 확인하고자 작성했습니다.
+	 * 각자 다른 회원들이 동시에 접근했을 때 동시성 이슈가 발생하는지 여부를 확인하고자 작성했습니다.
 	 */
 
 	@Test
-	void 회원_10명이_동시에_10_000_포인트를_충전했을_때_각자_10_000_포인트를_보유하게_된다() throws InterruptedException {
+	void 회원_10명이_동시에_자신이_가지고_있는_10000_포인트_중_5000_포인트를_사용할_경우_각각_5000_포인트씩_남게_된다() throws InterruptedException {
 		// given
 		final int threadCount = 10;
 		final long amountToCharge = 10_000L;
+		final long amountToUse = 5_000L;
+		final long pointBalance = amountToCharge - amountToUse;
+
+		for (long userId = 1; userId <= threadCount; userId++) {
+			// user 마다 10,000 포인트씩 충전한다.
+			userPointTable.insertOrUpdate(userId, amountToCharge);
+		}
 
 		CountDownLatch countDownLatch = new CountDownLatch(threadCount);
 		ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
@@ -83,8 +87,9 @@ public class PointChargeServiceIntegrationTest {
 		// when
 		IntStream.range(1, threadCount + 1)
 			.forEach((userId) -> executorService.execute(() -> {
-				pointChargeService.execute(new PointChargeService.Command(
-					userId, amountToCharge, System.currentTimeMillis()
+				// user 마다 5,000 포인트씩 사용한다.
+				pointUseService.execute(new PointUseService.Command(
+					userId, amountToUse, System.currentTimeMillis()
 				));
 				countDownLatch.countDown();
 			}));
@@ -94,7 +99,7 @@ public class PointChargeServiceIntegrationTest {
 		// then
 		for (long userId = 1; userId <= threadCount; userId++) {
 			UserPoint userPoint = userPointTable.selectById(userId);
-			assertThat(userPoint.point()).isEqualTo(amountToCharge);
+			assertThat(userPoint.point()).isEqualTo(pointBalance);
 		}
 
 	}
